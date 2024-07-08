2e33a0dc6bef14ec0198850cd9389670b5b6ec07
Md Amam Uddin Srabon <47981853+srabon0@users.noreply.github.com>
2024-07-09 01:14:42 +0600

    cors on

--- src/app.ts
+++ src/app.ts
@@ -18,42 +18,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(
   cors({
-    allowedHeaders: [
-      'Accept',
-      'Accept-Language',
-      'Authorization',
-      'Cache-Control',
-      'Content-Disposition',
-      'Content-Encoding',
-      'Content-Language',
-      'Content-Length',
-      'Content-MD5',
-      'Content-Range',
-      'Content-Type',
-      'Date',
-      'Host',
-      'If-Match',
-      'If-Modified-Since',
-      'If-None-Match',
-      'If-Unmodified-Since',
-      'Origin',
-      'OriginToken',
-      'Pragma',
-      'Range',
-      'Slug',
-      'Transfer-Encoding',
-      'Want-Digest',
-      'X-MediaBrowser-Token',
-      'X-Emby-Token',
-      'X-Emby-Client',
-      'X-Emby-Client-Version',
-      'X-Emby-Device-Id',
-      'X-Emby-Device-Name',
-      'X-Emby-Authorization',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     origin: '*',
-    // credentials: true, // If your front-end needs to send cookies to the server
+    credentials: true, // If your front-end needs to send cookies to the server
   }),
 );
 
